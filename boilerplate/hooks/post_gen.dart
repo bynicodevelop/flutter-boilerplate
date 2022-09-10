3e3316a3066a3bfaf7e163aff5eaa81163f108ad
nico-develop <contact@nico-develop.fr>
2022-09-10 18:58:54 +0200

    Merge de develop et intégration des de la gestion d'icons

--- boilerplate/hooks/post_gen.dart
+++ boilerplate/hooks/post_gen.dart
@@ -1,3 +1,33 @@
+import 'dart:io';
+
 import 'package:mason/mason.dart';
 
-Future<void> run(HookContext context) async {}
+Future<void> run(HookContext context) async {
+  context.logger.info('Installing flutter_launcher_icons dependencie...');
+  await Process.run(
+    'flutter',
+    [
+      'pub',
+      'add',
+      '--dev',
+      'flutter_launcher_icons',
+    ],
+    workingDirectory: './${context.vars["app_package_name"]}',
+  );
+
+  context.logger.info('Generate icons with flutter_launcher_icons...');
+  await Process.run(
+    'flutter',
+    [
+      'pub',
+      'run',
+      'flutter_launcher_icons:main',
+      '-v',
+      '-f',
+      'flutter_launcher_icons.yaml',
+    ],
+    workingDirectory: './${context.vars["app_package_name"]}',
+  );
+
+  context.logger.info('Flutter project created');
+}
